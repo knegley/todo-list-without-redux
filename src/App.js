2d0db90ad7c9de81c119082c650c0d71532810be
Kyle Negley <kpnegley@gmail.com>
2020-02-11 11:29:37 -0500

    was able to get add and delete to work

--- src/App.js
+++ src/App.js
@@ -1,64 +1,89 @@
-import React, { Component } from "react";
+import React, { useReducer, useRef } from "react";
 import "./index.css";
 import todosList from "./todos.json";
 
-class App extends Component {
-  state = {
-    todos: todosList
+const App = () => {
+  let toDos = { todosList }.todosList;
+  console.log(toDos);
+  console.log(toDos[0]);
+
+  const toDoRef = useRef();
+
+  const [toDo, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "add":
+          const addedToDo = {
+            userId: 1,
+            id: Math.floor(Math.random() * 100000),
+            title: toDoRef.current.value,
+            completed: false
+          };
+          console.log(toDoRef.current);
+          console.log(addedToDo);
+          console.log([...state, addedToDo]);
+
+          return [...state, addedToDo];
+
+        case "delete":
+          console.log(action.index);
+          return state.filter((value, index) => index !== action.index);
+
+        default:
+          return state;
+      }
+    },
+    [...toDos]
+  );
+
+  const keyDownHandler = event => {
+    if (event.key === "Enter") {
+      console.log("entered");
+      console.log(event.target.value);
+      console.log(toDoRef.current.value);
+      console.log([toDo[3].title]);
+      console.log(toDo);
+      dispatch({ type: "add" });
+    }
   };
-  render() {
-    return (
-      <section className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <input
-            className="new-todo"
-            placeholder="What needs to be done?"
-            autofocus
-          />
-        </header>
-        <TodoList todos={this.state.todos} />
-        <footer className="footer">
-          <span className="todo-count">
-            <strong>0</strong> item(s) left
-          </span>
-          <button className="clear-completed">Clear completed</button>
-        </footer>
-      </section>
-    );
-  }
-}
 
-class TodoItem extends Component {
-  render() {
-    return (
-      <li className={this.props.completed ? "completed" : ""}>
-        <div className="view">
-          <input
-            className="toggle"
-            type="checkbox"
-            checked={this.props.completed}
-          />
-          <label>{this.props.title}</label>
-          <button className="destroy" />
-        </div>
-      </li>
-    );
-  }
-}
+  // const handleDelete = event => {
+  //   console.log(event.target);
+  //   console.log(event.target.id);
+  //   let id = event.target.id;
+  //   console.log(id);
+  //   console.log(toDo);
 
-class TodoList extends Component {
-  render() {
-    return (
-      <section className="main">
-        <ul className="todo-list">
-          {this.props.todos.map(todo => (
-            <TodoItem title={todo.title} completed={todo.completed} />
-          ))}
-        </ul>
-      </section>
-    );
-  }
-}
+  //   const filtered = toDo.filter(value => value.id !== id);
+  //   console.log(filtered);
+  //   dispatch({ type: "delete", index });
+  //   return filtered;
+  // };
+
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        placeholder=" type to do"
+        // onKeyDown={() => dispatch({ type: "add" })}
+        onKeyDown={keyDownHandler}
+        ref={toDoRef}
+      ></input>
+      {toDo.map((item, index) => {
+        return (
+          <div key={item.id}>
+            {toDo[index].title}
+            <input
+              type="button"
+              value="Delete"
+              onClick={() => dispatch({ type: "delete", index })}
+            ></input>
+            <input type="checkbox"></input>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
+};
 
 export default App;
