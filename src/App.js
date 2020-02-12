0ffaa7535f541a73f7acf5429ff766740e28c989
Kyle Negley <kpnegley@gmail.com>
2020-02-12 02:14:14 -0500

    was able to finish features for part 1

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
 import React, { useReducer, useRef } from "react";
 import "./index.css";
-import todosList from "./todos.json";
+// import todosList from "./todos.json";
 
 const App = () => {
-  let toDos = { todosList }.todosList;
-  console.log(toDos);
-  console.log(toDos[0]);
+  // let toDos = { todosList }.todosList;
+  // console.log(toDos);
+  // console.log(toDos[0]);
+  // console.log(toDos[0].completed);
 
   const toDoRef = useRef();
 
-  const [toDo, dispatch] = useReducer(
-    (state, action) => {
-      switch (action.type) {
-        case "add":
-          const addedToDo = {
-            userId: 1,
-            id: Math.floor(Math.random() * 100000),
-            title: toDoRef.current.value,
-            completed: false
-          };
-          console.log(toDoRef.current);
-          console.log(addedToDo);
-          console.log([...state, addedToDo]);
+  const [toDo, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        const addedToDo = {
+          userId: 1,
+          id: Math.floor(Math.random() * 100000),
+          title: toDoRef.current.value,
+          completed: false
+        };
+        console.log(toDoRef.current);
+        console.log(addedToDo);
+        console.log([...state, addedToDo]);
 
-          return [...state, addedToDo];
+        return [...state, addedToDo];
 
-        case "delete":
-          console.log(action.index);
-          return state.filter((value, index) => index !== action.index);
+      case "delete":
+        console.log(action.index);
+        return state.filter((value, index) => index !== action.index);
 
-        default:
-          return state;
-      }
-    },
-    [...toDos]
-  );
+      case "markComplete":
+        state[action.index].completed = !state[action.index].completed;
+        return [...state];
+
+      case "deleteComplete":
+        return state.filter(value => value.completed === false);
+
+      default:
+        return state;
+    }
+  }, []);
 
   const keyDownHandler = event => {
     if (event.key === "Enter") {
       console.log("entered");
       console.log(event.target.value);
       console.log(toDoRef.current.value);
-      console.log([toDo[3].title]);
+
       console.log(toDo);
+
       dispatch({ type: "add" });
     }
   };
@@ -65,20 +71,34 @@ const App = () => {
       <input
         type="text"
         placeholder=" type to do"
+        autoFocus
         // onKeyDown={() => dispatch({ type: "add" })}
         onKeyDown={keyDownHandler}
         ref={toDoRef}
       ></input>
+      <input
+        type="button"
+        value="delete completed"
+        onClick={() => dispatch({ type: "deleteComplete" })}
+      ></input>
       {toDo.map((item, index) => {
         return (
-          <div key={item.id}>
+          <div
+            key={item.id}
+            // style={
+            //   toDo[index].completed && { textDecorationLine: "line-through" }
+            className={toDo[index].completed ? "completed" : null}
+          >
             {toDo[index].title}
             <input
               type="button"
               value="Delete"
               onClick={() => dispatch({ type: "delete", index })}
             ></input>
-            <input type="checkbox"></input>
+            <input
+              type="checkbox"
+              onClick={() => dispatch({ type: "markComplete", index })}
+            ></input>
           </div>
         );
       })}
