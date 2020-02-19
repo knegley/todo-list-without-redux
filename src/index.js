59dedd9ed5235b6cadbbf6eb41336aefc8d04da7
Kyle Negley <kpnegley@gmail.com>
2020-02-19 11:10:06 -0500

    removed < from index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    >
     <App />
   </BrowserRouter>,
   document.getElementById("root")
